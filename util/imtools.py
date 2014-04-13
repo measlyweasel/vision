921a235690aa8640fb30302eb3a0d90137d843d3
jason cahoon <jfcahoon@gmail.com>
2014-04-13 09:53:19 -0500

    added function to imtools that computes averages of a list of images

--- util/imtools.py
+++ util/imtools.py
@@ -23,13 +23,36 @@ def histeq(image, nBins=256):
     """
     Histogram equalization of a grayscale image
     """
-    #get image histogram
+    # get image histogram
     imhist, bins = histogram(image.flatten(), nBins, density=True)
-    cdf = imhist.cumsum() # cumulative distribution function
-    cdf = 255 * cdf / cdf[-1] #normalize
+    cdf = imhist.cumsum()  # cumulative distribution function
+    cdf = 255 * cdf / cdf[-1]  #normalize
 
     #linear interpolation of cdf to find new pixel values
     im2 = interp(image.flatten(), bins[:-1], cdf)
 
     #reshape is a cool trick to unflatten the array
     return im2.reshape(image.shape), cdf
+
+
+def compute_average(imageList):
+    """
+    Compute the average of a list of images
+    """
+
+    #initialize the output array with zeros
+    imageShape = imageList[0].size
+    averageim = zeros(imageShape,'f')
+
+    imageCount = 0
+    for image in imageList:
+        try:
+            averageim+=array(Image.open(image))
+            ++imageCount
+        except:
+            print(image + '...skipped')
+
+    averageim /= imageCount
+
+    #pixel values can't be floats
+    return array(averageim,'uint8')
