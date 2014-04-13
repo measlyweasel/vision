7bf13107386e41297973ff1da7fc0e03025e5b5d
jason cahoon <jfcahoon@gmail.com>
2014-04-13 09:54:09 -0500

    better comments in ch1/graylevelTransforms

--- ch1/graylevelTransforms.py
+++ ch1/graylevelTransforms.py
@@ -1,15 +1,18 @@
 from PIL import Image
 from pylab import *
 
+#original image
 image = array(Image.open('data/empire.jpg').convert('L'))
 gray()
 
+#invert image
 image2 = 255-image
 
+#clamp between 100 and 200
 image3 = (100.0/255)*image+100
 
+#square and normalize
 image4 = 255.0 * (image/255)**2
-
 
 imshow(image)
 
