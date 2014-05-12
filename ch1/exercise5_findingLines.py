ecbfe6738a83651a1050eaf99adba53dd4afd5fc
jason cahoon <jfcahoon@gmail.com>
2014-05-11 22:24:36 -0500

    totally rad, fixed up the connected component algorithm and have spot checked it

--- ch1/exercise5_findingLines.py
+++ ch1/exercise5_findingLines.py
@@ -47,7 +47,7 @@ isSimilarVectorized = vectorize(isSimilar)
 #only need to go through half the bins since each straight line will have gradients
 #pointing opposing directions at the line. Really only need to check half of the compass
 #points because of this
-for bin in range(1, int(ceil(len(bins)/2))):  #one based indexing of bins
+for bin in [1]:#range(1, int(ceil(len(bins)/2))):  #one based indexing of bins
     print("bin ", bin)
     similarAngledPoints = isSimilarVectorized(binnedAngles.reshape(im.shape), bin=bin)
 
@@ -57,36 +57,40 @@ for bin in range(1, int(ceil(len(bins)/2))):  #one based indexing of bins
     #now to find the connected components
 
     compassDirections = list(itertools.product([1,0,-1],repeat=2))
+    compassDirections.remove((0,0))
 
     visited = zeros(similarAngledPoints.shape,dtype=bool)
 
     #depth first search
     def dfs(yPoint,xPoint):
-        if not similarAngledPoints[yPoint][xPoint]:
+        if not similarAngledPoints[yPoint][xPoint] or visited[yPoint][xPoint]:
             return []
-        connectedComponent = [(yPoint,xPoint)]
+        connectedComponent=[]
         stack = []
         stack.append((yPoint,xPoint))
-        while len(stack) > 0:
+        while stack:
             pixel = stack.pop()
             y = pixel[0]
             x = pixel[1]
             if not visited[y][x]:
                 visited[y][x] = True
-                for direction in compassDirections:
-                    nextY = y+direction[0]
-                    nextX = x+direction[1]
-                    if similarAngledPoints[nextY][nextX]:
-                        connectedComponent.append((nextY,nextX))
-                        stack.push((nextY,nextX))
+                if similarAngledPoints[y][x]:
+                    connectedComponent.append((y,x))
+                    for direction in compassDirections:
+                        nextY = y+direction[0]
+                        nextX = x+direction[1]
+
+                        if nextY<0 or nextX<0 or nextY>=similarAngledPoints.shape[0] or nextX>=similarAngledPoints.shape[1]:
+                            continue
+
+                        if similarAngledPoints[nextY][nextX] and not visited[nextY][nextX]:
+                            stack.append((nextY,nextX))
         return connectedComponent
 
-
     for (y,x),value in ndenumerate(similarAngledPoints):
-        if not visited[y][x]:
-            visited[y][x] = True
-            for direction in compassDirections:
-                [y+direction[0]][x+direction[1]]
+        connectedComponent = dfs(y,x)
+        if connectedComponent:
+            print(y,x,connectedComponent)
 
 
 show()
